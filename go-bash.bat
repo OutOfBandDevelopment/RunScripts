b27001f895c64b0104e5df4985acc47b62e5161d
Matthew Whited <mwhited@eliassen.com>
2024-02-05 12:14:42 -0500

    add some more persistence

--- go-bash.bat
+++ go-bash.bat
@@ -1,8 +1,9 @@
 @docker volume create go-pkg
-
+@docker volume create go-build-cache
 @docker run --rm -it ^
 -v %cd%:/usr/src/ ^
 -v go-pkg:/go/pkg ^
+-v go-build-cache:/root/.cache/go-build/ ^
 -w /usr/src/ ^
 -p 8080:8080 ^
 golang:latest bash %*
--- rustup.bat
+++ rustup.bat
@@ -1,2 +1,7 @@
-
-@docker run --rm -it -v %cd%:/usr/src/ -w /usr/src/ rust:latest rustup %*
+@docker volume create rust-rustup
+@docker run ^
+--rm -it ^
+-v %cd%:/usr/src/ ^
+-v rust-rustup:/usr/local/rustup/ ^
+-w /usr/src/ ^
+rust:latest rustup %*
