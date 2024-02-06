cd6fec8c2b613f014b9855a732fdf527cf2248c6
Matthew Whited <matt@whited.us>
2024-02-05 22:30:43 -0500

    add a gui script for google-chrome

---
+++ MorePower/notes.md
@@ -0,0 +1,5 @@
+
+* https://www.composerize.com/
+* https://learn.microsoft.com/en-us/windows/wsl/tutorials/gui-apps
+* https://github.com/microsoft/wslg
+  
---
+++ google-chrome-bash.bat
@@ -0,0 +1,27 @@
+@ECHO OFF
+SETLOCAL
+
+IF EXIST %CD%\before_docker.cmd CALL %CD%\before_docker.cmd
+IF EXIST %CD%\before_docker.bat CALL %CD%\before_docker.bat
+
+SET SCRIPT_ROOT=%~dp0
+
+docker build -t oobdev/google-chrome -f %SCRIPT_ROOT%MorePower\DockerFile.google-chrome %SCRIPT_ROOT%
+
+docker volume create google-chrome-cache >NUL 2>&1
+docker volume create google-chrome-config >NUL 2>&1
+
+docker run --rm -it ^
+-v /run/desktop/mnt/host/wslg/.X11-unix:/tmp/.X11-unix ^
+-v /run/desktop/mnt/host/wslg:/mnt/wslg ^
+-v google-chrome-cache:/root/.cache/google-chrome ^
+-v google-chrome-config:/root/.config/google-chrome %EXTRA_DOCKER_COMMANDS% ^
+oobdev/google-chrome bash
+
+SET LAST_ERROR=%ERRORLEVEL%
+
+IF EXIST %CD%\after_docker.cmd CALL %CD%\after_docker.cmd
+IF EXIST %CD%\after_docker.bat CALL %CD%\after_docker.bat
+
+ENDLOCAL
+EXIT /B %ERRORLEVEL%
