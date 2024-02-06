f601b95221b55ca2e4cb79043c41e9cdcd83688f
Matthew Whited <matt@whited.us>
2024-02-05 23:55:46 -0500

    add ruby support

---
+++ rails.bat
@@ -0,0 +1,23 @@
+@ECHO OFF
+SETLOCAL
+
+IF EXIST %CD%\before_docker.cmd CALL %CD%\before_docker.cmd
+IF EXIST %CD%\before_docker.bat CALL %CD%\before_docker.bat
+
+@docker volume create ruby-root-local >NUL 2>&1
+@docker volume create ruby-usr-local  >NUL 2>&1
+@docker run --rm -it ^
+-v %cd%:/usr/src/ ^
+-v ruby-root-local:/root/.local/ ^
+-v ruby-usr-local:/usr/local/ ^
+-w /usr/src/ ^
+-p 8080:8080 %EXTRA_DOCKER_COMMANDS% ^
+ruby:latest rails %*
+
+SET LAST_ERROR=%ERRORLEVEL%
+
+IF EXIST %CD%\after_docker.cmd CALL %CD%\after_docker.cmd
+IF EXIST %CD%\after_docker.bat CALL %CD%\after_docker.bat
+
+ENDLOCAL
+EXIT /B %ERRORLEVEL%
