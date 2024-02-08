4376018a7ba98dfe24134301863c5a32a3a47821
Matthew Whited <mwhited@eliassen.com>
2024-02-07 19:15:30 -0500

    clean up script

--- MorePower/template_commands/build_scripts.js
+++ MorePower/template_commands/build_scripts.js
@@ -3,7 +3,7 @@ const yaml = require('js-yaml');
 const Handlebars = require('handlebars');
 const os = require('os');
 
-console.log('Hostname:',  os.hostname());
+console.log('Hostname:', os.hostname());
 console.log('Operating System Platform:', os.platform());
 
 const script_path = os.platform() === 'win32' ? process.cwd() + "\\..\\..\\" : "/usr/src/templates/";
@@ -13,21 +13,21 @@ function deepClone(obj) {
 }
 
 function buildPurgeAll(input, purge_commands) {
-  const result = purge_commands.map(m=>(input.isDosPaths ? 'CALL ' : '') + m).join(os.platform() === 'win32' ? '\r\n' : '\n');
-  
+  const result = purge_commands.map(m => (input.isDosPaths ? 'CALL ' : '') + m).join(os.platform() === 'win32' ? '\r\n' : '\n');
+
   const script_out_file = `purge-all${input.scriptExtension}`;
-  console.log(`---: ${script_out_file} :---------------------------------------`);      
+  console.log(`---: ${script_out_file} :---------------------------------------`);
   const filename = `${script_path}${script_out_file}`;
   console.log(`sourceData: purge-all: ${filename}`, purge_commands);
-  
+
   fs.writeFileSync(filename, result);
 }
 
-function buildPurgeScript(input, platform, sourceData, template, script_path) {    
+function buildPurgeScript(input, platform, sourceData, template, script_path) {
   sourceData.command_file = 'purge-' + platform.name;
-  
+
   const script_out_file = `${sourceData.command_file}${input.scriptExtension}`;
-  console.log(`---: ${script_out_file} :---------------------------------------`);      
+  console.log(`---: ${script_out_file} :---------------------------------------`);
   const filename = `${script_path}${script_out_file}`;
   console.log(`sourceData: ${sourceData.command_file}: ${filename}`, sourceData);
 
@@ -43,26 +43,31 @@ function buildCommandScripts(input, platform, template, script_path) {
   platform.commands.forEach(command => {
     sourceData = deepClone(platform);
     delete sourceData.commands;
-    
+
     sourceData.command_file = command.name || Object.keys(command)[0];
     sourceData.command = command.cmd || command[sourceData.command_file];
-    
-    //console.log(`>>>>> cmd`, platform, command, Object.keys(command)[0], sourceData)
 
-    var create_volumes = {};
-    var volumes = {};
-    for (const [idx, obj] of Object.entries(platform.volumes)) {
-      for (const [key, value] of Object.entries(obj)) {
-        if (key !== 'no_create')
-          volumes[key === 'CURRENT_DIRECTORY' ? input.currentPathVariable : key] = value;
-        if (!obj.no_create)
-          create_volumes[key] = value;
-      }
-    }
-    sourceData.create_volumes = create_volumes;
-    sourceData.volumes = volumes;
-
+    sourceData.create_volumes = {};
+    sourceData.volumes = {};
     sourceData.ports = {};
+
+    for (const [idx, volume] of Object.entries(platform.volumes)) {
+
+      const volume_key = volume.name || Object.keys(volume)[0];
+      const volume_path = volume.path || volume[volume_key];
+      const real_volume_key = volume_key === 'CURRENT_DIRECTORY' ? input.currentPathVariable : volume_key;
+
+      //console.log(`>>>>> vol "${real_volume_key}:${volume_path}"`, volume);
+
+      sourceData.volumes[real_volume_key] = volume_path;
+      if (!volume.no_create) {
+        sourceData.create_volumes[real_volume_key] = volume_path;
+      }
+      // else {
+      //   console.log(`>>>>>>> vol "${real_volume_key}:${volume_path}" no create`, volume);
+      // }
+    }
+
     if ('ports' in platform) {
       for (const [idx, obj] of Object.entries(platform.ports)) {
         for (const [key, value] of Object.entries(obj)) {
@@ -79,7 +84,7 @@ function buildCommandScripts(input, platform, template, script_path) {
     }
 
     const script_out_file = `${sourceData.command_file}${input.scriptExtension}`;
-    console.log(`---: ${script_out_file} :---------------------------------------`);      
+    console.log(`---: ${script_out_file} :---------------------------------------`);
     const filename = `${script_path}${script_out_file}`;
     console.log(`sourceData: ${sourceData.command_file}: ${filename}`, sourceData);
 
@@ -97,40 +102,44 @@ function scriptBuilder(
   const data = yaml.load(yamlData);
 
   const source = fs.readFileSync(input.templateSource, 'utf8');
-  const template = Handlebars.compile(source);  
+  const template = Handlebars.compile(source);
 
   const purgeSource = fs.readFileSync(input.purgeTemplateSource, 'utf8');
-  const purgeTemplate = Handlebars.compile(purgeSource);  
+  const purgeTemplate = Handlebars.compile(purgeSource);
 
   let purge_commands = [];
 
-  data.platforms.forEach(platform => {
-    console.log(`===: ${platform.name} :=======================================`);
-    if ('docker_file' in platform) {
-      platform.docker_file = platform.docker_file.replace('{SCRIPT_ROOT}', input.scriptRootVariable);
-      if (input.isDosPaths) {
-        platform.docker_file = platform.docker_file.replace('/', '\\');
-      } else {
-        platform.docker_file = platform.docker_file.replace('\\', '/');
+  if (data.platforms) {
+    data.platforms.forEach(platform => {
+      console.log(`===: ${platform.name} :=======================================`);
+      if ('docker_file' in platform) {
+        platform.docker_file = platform.docker_file.replace('{SCRIPT_ROOT}', input.scriptRootVariable);
+        if (input.isDosPaths) {
+          platform.docker_file = platform.docker_file.replace('/', '\\');
+        } else {
+          platform.docker_file = platform.docker_file.replace('\\', '/');
+        }
       }
-    }
-    console.log(`platform: ${platform.name}: `, platform);
-    
-    const sourceData = buildCommandScripts(input, platform, template, script_path);
-    const purge_command = buildPurgeScript(input, platform, sourceData, purgeTemplate, script_path);
-    purge_commands.push(purge_command);
-  });
-  
-  buildPurgeAll(input, purge_commands);
+      console.log(`platform: ${platform.name}: `, platform);
+
+      const sourceData = buildCommandScripts(input, platform, template, script_path);
+      const purge_command = buildPurgeScript(input, platform, sourceData, purgeTemplate, script_path);
+      purge_commands.push(purge_command);
+    });
+
+    buildPurgeAll(input, purge_commands);
+  } else {
+    console.error(`no platforms defined`, data);
+  }
 }
 
 // build batch files
 scriptBuilder({
   'templateData': 'command_def.yaml',
   'templateSource': 'command_script.hbs',
-  'scriptRootVariable':  '%SCRIPT_ROOT%',
+  'scriptRootVariable': '%SCRIPT_ROOT%',
   'isDosPaths': true,
-  'currentPathVariable':   '%cd%',
+  'currentPathVariable': '%cd%',
   'scriptExtension': '.bat',
   'purgeTemplateSource': 'purge_script.hbs'
 });
@@ -139,9 +148,9 @@ scriptBuilder({
 scriptBuilder({
   'templateData': 'command_def.yaml',
   'templateSource': 'shell_script.hbs',
-  'scriptRootVariable':  '$SCRIPT_ROOT/',
+  'scriptRootVariable': '$SCRIPT_ROOT/',
   'isDosPaths': false,
-  'currentPathVariable':   '$PWD',
+  'currentPathVariable': '$PWD',
   'scriptExtension': '',
   'purgeTemplateSource': 'purge_script.hbs'
 });
