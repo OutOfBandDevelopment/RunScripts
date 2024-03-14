e2aae0c5fc49383b562f4f1c902f7037f57b77ac
Matthew Whited <matt@whited.us>
2024-03-14 16:13:19 -0400

    update juypter and openwebui notes

--- MorePower/extra_commands/juypter.bat
+++ MorePower/extra_commands/juypter.bat
@@ -4,10 +4,13 @@ docker build ^
 --file ..\DockerFile.jupyter ^
 ..
 
+IF NOT ERRORLEVEL 0 GOTO :EOF
+
 docker run --rm %EXTRA_DOCKER_COMMANDS% ^
 --interactive ^
 --tty ^
 --volume %cd%:/jupyter/src/ ^
 --workdir /jupyter/src/ ^
 --publish 8888:8888 ^
+--gpus all ^
 oobdev/jupyter  %*
--- MorePower/extra_commands/openwebui.bat
+++ MorePower/extra_commands/openwebui.bat
@@ -2,7 +2,7 @@
 docker run ^
 --rm ^
 -p 3001:8080 ^
--e OLLAMA_API_BASE_URL=http://172.17.0.4:11434/api ^
+-e OLLAMA_API_BASE_URL=http://172.17.0.2:11434/api ^
 -v open-webui:/app/backend/data ^
 --name open-webui ^
 ghcr.io/open-webui/open-webui:main
